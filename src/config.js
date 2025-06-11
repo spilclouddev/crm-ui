521fe3d99a17a7a853eec57bd327b60d7d287b70
vinujajay <vinuja@spil.com.au>
2025-06-11 14:33:44 +0530

    url

--- src/config.js
+++ src/config.js
@@ -1,9 +1,9 @@
 // src/config.js
 const config = {
-  API_URL: "https://crm-be.fly.dev/",
+  API_URL: "https://crm-ui.fly.dev/",
   // Add other configuration variables as needed
 };
 //http://localhost:5000/api
-//https://crm-be.fly.dev/
+//https://crm-ui.fly.dev/
 
 export default config;
